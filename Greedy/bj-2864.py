b95e30048e20b1c8c99ab8928134cec46d668dff
Minwoo Lee <dlalsdn1009@naver.com>
2022-06-26 00:58:01 +0900

    https://www.acmicpc.net/problem/2864

--- Greedy/bj-2864.py
+++ Greedy/bj-2864.py
@@ -1,7 +1,6 @@
 # https://www.acmicpc.net/problem/2864
 
-a, b = input().split()
-
-min_a, min_b = a.replace('6', '5'), b.replace('6', '5')
-max_a, max_b = a.replace('5', '6'), b.replace('5', '6')
-print(int(min_a)+int(min_b), int(max_a)+int(max_b))
+cnt = int(input())
+data = input()
+result = len(data) - data.count("LL") + 1
+print(cnt if cnt <= result else result)
