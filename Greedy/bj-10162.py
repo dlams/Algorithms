160b4e38f903adeeec4ff0877768d2bd6bce77fa
Minwoo Lee <dlalsdn1009@naver.com>
2022-06-26 00:35:52 +0900

    https://www.acmicpc.net/problem/10162

--- Greedy/bj-10162.py
+++ Greedy/bj-10162.py
@@ -1,18 +1,7 @@
 # https://www.acmicpc.net/problem/10162
 
 t = int(input())
-table = [300, 60, 10]
-result = [0, 0, 0]
-cnt = 0
-# result = 0
-for i in table:
-    result[cnt] = t//i
-    t = t%i
-    cnt += 1
-
-if t != 0:
+if (t%10) != 0:
     print(-1)
 else:
-    print(result[0], result[1], result[2])
-    # target = (t//300)
-    # r
+    print(f'{t//300} {t%300//60} {t%60//10}')
